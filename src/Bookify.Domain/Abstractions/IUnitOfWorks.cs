c7f01bfecbc09fb13ed499d624410e903f1c6f23
hubedid <hubed@protonmail.com>
2024-05-04 11:31:54 +0700

    [ADD] repository

---
+++ src/Bookify.Domain/Abstractions/IUnitOfWorks.cs
@@ -0,0 +1,7 @@
+﻿namespace Bookify.Domain.Abstractions
+{
+    internal interface IUnitOfWorks
+    {
+        Task<int> SaveChangeAsync(CancellationToken cancellationToken = default); 
+    }
+}
---
+++ src/Bookify.Domain/Apartments/IApartmentRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Bookify.Domain.Apartments
+{
+    public interface IApartmentRepository
+    {
+        Task<Apartment> GetByIdAsync(Guid Id, CancellationToken cancellationToken = default);
+    }
+}
