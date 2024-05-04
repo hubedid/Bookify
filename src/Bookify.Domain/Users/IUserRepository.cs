1cb93a16d04290b96414decaadf30770eebadb18
hubedid <hubed@protonmail.com>
2024-05-04 11:30:51 +0700

    [ADD] user domain entity

---
+++ src/Bookify.Domain/Users/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookify.Domain.Users
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByIdAsync(Guid Id, CancellationToken cancellationToken = default);
+        void Add(User user);
+    }
+}
