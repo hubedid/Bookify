ccc7f8d328b4c885b4e349e7a88adf7adf7f22ac
hubedid <hubed@protonmail.com>
2024-05-04 11:31:20 +0700

    [ADD] domain event interface

--- src/Bookify.Domain/Abstractions/Entity.cs
+++ src/Bookify.Domain/Abstractions/Entity.cs
@@ -2,10 +2,27 @@
 {
     public abstract class Entity
     {
+        private readonly List<IDomainEvents> _domainEvents = new();
+
         protected Entity(Guid id)
         {
             Id = id;
         }
         public Guid Id { get; init; }
+
+        public IReadOnlyList<IDomainEvents> GetDomainEvents()
+        {
+            return _domainEvents.ToList();
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+
+        public void RaiseDomainEvent(IDomainEvents domainEvents)
+        {
+            _domainEvents.Add(domainEvents);
+        }
     }
 }
